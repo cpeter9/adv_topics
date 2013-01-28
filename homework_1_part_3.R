1959737dc21771011e02af5866156373cc07f11d
Chris <Peters>
2013-01-27 18:15:02 -0600

    First commit of part 3.

---
+++ homework_1_part_3.R
@@ -0,0 +1,16 @@
+# Author: Christopher Peters
+# Email: cpeter9@gmail.com
+# Homework 1: simulation
+
+# 3. Receiver operating characteristic curve
+
+data <- as.data.frame(list(T = c(0.062, 0.177, 0.202, 0.206, 0.266, 
+                                 0.372, 0.380, 0.384, 0.498, 0.573,
+                                 0.629, 0.661, 0.687, 0.718, 0.770,
+                                 0.777, 0.898, 0.908, 0.945, 0.992),
+                           D = c(0, 0, 0, 0, 0,
+                                 0, 0, 0, 1, 1,
+                                 1, 1, 0, 1, 1,
+                                 0, 1, 1, 0, 1)))
+
+
