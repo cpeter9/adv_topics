b930319b96b59cf0b40895372e2aa3637d3f321c
Chris <Peters>
2013-02-08 08:47:56 -0600

    Almost finished with 2a.

--- homework_2/cpp_homework_2.R
+++ homework_2/cpp_homework_2.R
@@ -1,3 +1,30 @@
 # Author: Christopher Peters
 # Email: cpeter9@gmail.com
 # Homework 2: More simulation
+
+# install.packages("MASS")
+library(MASS)
+
+setwd("C:/R_stuff/adv_topics/homework_2/")
+
+data <- read.csv("hw1_data1.csv", stringsAsFactors = FALSE)
+
+# Code date as an object of class POSIX
+data$Date <- as.Date(data$Date)
+
+# Reverse sort of data
+data <- data[order(data$Date), ]
+
+# Diff and log close price
+data$returns <- 0
+
+for(i in 2:length(data$Date)){
+  data$returns[i] <- log(data$Close[i] / data$Close[i - 1])
+}
+
+# Fit normal distribution to returns
+fit <- fitdistr(data$returns, "normal")
+
+mean <- fit$estimate[1]
+sd <- fit$estimate[2]
+
